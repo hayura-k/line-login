c7f422f5a1d13e477af483ebb2121e38075bdcf9
yuki hara <hara.0509@icloud.com>
2021-09-11 16:04:20 +0900

    lineログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'users#index'
+  resources :users
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback"
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 end
